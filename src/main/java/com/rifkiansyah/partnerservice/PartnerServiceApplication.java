a190d1928acfc8e0655fc0aab65e5b5122363a90
sheizan <adfx-02morgan>
2019-09-16 11:48:26 +0700

    add eureka client and resource

--- src/main/java/com/rifkiansyah/partnerservice/PartnerServiceApplication.java
+++ src/main/java/com/rifkiansyah/partnerservice/PartnerServiceApplication.java
@@ -2,8 +2,12 @@ package com.rifkiansyah.partnerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.rifkiansyah.partnerservice.repository")
+@EnableEurekaClient
 public class PartnerServiceApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/rifkiansyah/partnerservice/resource/PartnerResource.java
+++ src/main/java/com/rifkiansyah/partnerservice/resource/PartnerResource.java
@@ -1,8 +1,36 @@
 package com.rifkiansyah.partnerservice.resource;
 
+import com.netflix.discovery.converters.Auto;
 import com.rifkiansyah.partnerservice.model.Partner;
+import com.rifkiansyah.partnerservice.repository.PartnerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/rest/partner")
 public class PartnerResource {
 
+    PartnerRepository partnerRepository;
 
+    @Autowired
+    RestTemplate restTemplate;
+
+    public PartnerResource(PartnerRepository partnerRepository){
+        this.partnerRepository = partnerRepository;
+    }
+
+    public List<Partner> getAllPartners(){
+        return partnerRepository.findAll();
+    }
+
+    public Partner getPartnerByName(String partnerName){
+        return partnerRepository.findOneByPartnerName(partnerName);
+    }
+
+    //public Partner getAllPartnerItems(String partnerName)
+    //#TODO get all partner item from inventory service
 }
